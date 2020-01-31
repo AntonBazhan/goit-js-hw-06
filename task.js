b8ff2e5ca873ad6c4154f9c31a13bcaf2d1e374b
AntonBazhan <51802003+AntonBazhan@users.noreply.github.com>
2020-02-01 00:27:12 +0200

    9

--- task.js
+++ task.js
@@ -3,18 +3,13 @@
 import users from "./users.js";
 
 // Задание 1
-const getUserNames = users => {
-  const names = users.map(user => user.name);
-  return names;
-};
+const getUserNames = users =>  users.map(user => user.name);
 console.log(getUserNames(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
 
 // Задание 2
-const getUsersWithEyeColor = (users, color) => {
-  const blueEye = users.filter(user => user.eyeColor === color);
-  return blueEye;
-};
+const getUsersWithEyeColor = (users, color) =>  users.filter(user => user.eyeColor === color);
+ 
 console.log(getUsersWithEyeColor(users, "blue")); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
 
 // Задание 3
@@ -25,38 +20,24 @@ const getUsersWithGender = (users, gender) => {
 console.log(getUsersWithGender(users, "male")); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
 
 // Задание 4
-const getInactiveUsers = users => {
-  const activeUsers = users.filter(user => user.isActive);
-  return activeUsers;
-};
+const getInactiveUsers = users =>  users.filter(user => user.isActive);
 console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
 
 // Задание 5
-const getUserWithEmail = (users, email) => {
-  const user = users.find(user => user.email === email);
-  return user;
-};
+const getUserWithEmail = (users, email) =>  users.find(user => user.email === email);
 console.log(getUserWithEmail(users, "shereeanthony@kog.com")); // {объект пользователя Sheree Anthony}
 console.log(getUserWithEmail(users, "elmahead@omatom.com")); // {объект пользователя Elma Head}
 
 // Задание 6
-const getUsersWithAge = (users, min, max) => {
-  const filterAge = users.filter(user => min <= user.age && user.age <= max);
-  return filterAge;
-};
+const getUsersWithAge = (users, min, max) => users.filter(user => min <= user.age && user.age <= max);
 console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
 console.log(getUsersWithAge(users, 30, 40));
 // [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
 
 // Задание 7
 
-const calculateTotalBalance = users => {
-  const summ = users.reduce(
-    (totalBalance, user) => totalBalance + user.balance,
-    0
-  );
-  return summ;
-};
+const calculateTotalBalance = users => users.reduce(
+    (totalBalance, user) => totalBalance + user.balance,0);
 console.log(calculateTotalBalance(users)); // 20916
 
 // Задание 8
